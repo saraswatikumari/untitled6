663806bbb2a7a6656aedbcbf827f8be1353282fe
saraswatikumari <saraswati.kumari_cs19@gla.ac.in>
2021-11-18 20:11:32 +0530

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,24 @@
+public class test {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+          /*  if (i == 5)
+            {
+                continue;
+                 // break;
+            } else {
+                System.out.println(i + "  ");
+
+            }*/
+            if (i == 5)
+            {
+               // continue;
+                 break;
+            } else {
+                System.out.println(i + "  ");
+
+            }
+        }
+    }
+}
+
